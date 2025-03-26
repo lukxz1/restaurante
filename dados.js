70f04755a73e1cc16dc5b275496761ff64142322
Lukxz <131976724+lukxz1@users.noreply.github.com>
2025-03-26 00:46:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,62 @@
+let dados = [
+    {   
+        nome : "Pizza",
+        descricao : "A pizza é um prato de origem italiana, feito com uma base de massa coberta com molho de tomate, queijo e diversos ingredientes como carnes, vegetais e especiarias.",
+        link : "https://pt.wikipedia.org/wiki/Pizza",
+        imagem: "img/pizza.png" 
+    },
+    {
+        nome : "Sushi",
+        descricao : "O sushi é um prato japonês que consiste em arroz temperado com vinagre, combinado com peixe cru, frutos do mar ou vegetais, frequentemente servido com molho de soja.",
+        link : "https://pt.wikipedia.org/wiki/Sushi",
+        imagem: "img/sushi.png"
+    },
+    {
+        nome : "Tacos",
+        descricao : "Os tacos são um prato tradicional mexicano, consistindo em uma tortilla dobrada ou enrolada, recheada com carne, feijão, queijo, alface e outros ingredientes.",
+        link : "https://pt.wikipedia.org/wiki/Taco",
+        imagem: "img/tacos.png"
+    },
+    {
+        nome : "Hambúrguer",
+        descricao : "O hambúrguer é um prato popular que consiste em um disco de carne moída, geralmente bovina, servido em um pão, frequentemente acompanhado de queijo, alface, tomate e molhos.",
+        link : "https://pt.wikipedia.org/wiki/Hamb%C3%BArguer",
+        imagem: "img/hamburguer.jpg"
+    },
+    {
+        nome : "Shawarma",
+        descricao : "Shawarma é um prato do Oriente Médio feito com carne assada em espeto vertical, servida em pão sírio com molhos e vegetais.",
+        link : "https://pt.wikipedia.org/wiki/Xauarma",
+        imagem: "img/Shawarma.jpg"
+    },
+    {
+        nome : "Curry",
+        descricao : "O curry é um prato de origem indiana, feito com uma mistura de especiarias e ervas, geralmente cozido com carne, legumes ou grãos, e servido com arroz.",
+        link : "https://pt.wikipedia.org/wiki/Curry",
+        imagem: "img/curry.png"
+    },
+    {
+        nome : "Churrasco",
+        descricao : "O churrasco é um método de cozinhar carne, geralmente em uma grelha ou churrasqueira, popular em várias culturas, especialmente na América Latina.",
+        link : "https://pt.wikipedia.org/wiki/Churrasco",
+        imagem: "img/churrasco.png"
+    },
+    {
+        nome : "Crepe",
+        descricao : "O crepe é uma fina panqueca de origem francesa, que pode ser recheada com ingredientes doces ou salgados, como frutas, chocolate, queijo ou presunto.",
+        link : "https://pt.wikipedia.org/wiki/Crepe",
+        imagem: "img/crepe.jpg"
+    },
+    {
+        nome : "Bolo de Chocolate",
+        descricao : "O bolo de chocolate é uma sobremesa feita com massa de bolo que contém chocolate, frequentemente coberta com glacê de chocolate ou recheios variados.",
+        link : "https://pt.wikipedia.org/wiki/Bolo_de_chocolate",
+        imagem: "img/bolo.png"
+    },
+    {
+        nome : "Gelato",
+        descricao : "O gelato é um tipo de sorvete italiano, conhecido por sua textura cremosa e sabores intensos, feito com leite, açúcar e ingredientes naturais.",
+        link : "https://pt.wikipedia.org/wiki/Gelato",
+        imagem: "img/gelato.jpg"
+    }
+];
